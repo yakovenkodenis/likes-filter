cd4d9b13097dfc047b038a5a35e66030d0510dd0
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2016-11-28 13:03:45 +0200

    Set up basic popup page and implement vk url parser

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,46 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+const currentURL = document.location.search;
+
+console.log(currentURL);
+
+const startFilter = () => {
+    console.log('Start Filter Action');
+}
+
+const parseURL = url => {
+    const parsed = decodeURIComponent(url);
+    const params = parsed.split(/\?\w\=|\_|\//g)
+                    .filter(e => ["rev", ""].indexOf(e) < 0);
+
+    if (params[0].indexOf("wall") >= 0) { // community post or comment
+        params[0] = -params[0].split('-')[1];
+    
+        if (params.length === 2) {
+            return {
+            type: "post", owner_id: params[0], item_id: params[1]
+            };
+        } else if (params.length === 3) {
+            return {
+            type: "comment", owner_id: params[0], item_id: params[2].slice(1)
+            };
+        }
+    } else if (params[0].indexOf("photo") >= 0)   { // photo
+        return {
+            type: "photo", owner_id: params[0].split("photo")[1], item_id: params[1]
+        }
+    } else {
+        return {
+            error: "You cannot filter likes on this page"    
+        };
+    }
+}
+
+const onRequest = (request, sender, sendResponse) => {
+    if (request.action == 'filter') {
+        startFilter();
+    }
+    sendResponse({ 'response': 'HELLOO', currentURL });
+}
+
+chrome.extension.onRequest.addListener(onRequest);
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+const form = document.getElementById('form');
+const data = new FormData(form);
+
+console.log('POPUUP');
+console.log(data.entries());
+
+chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    const currentTab = tabs[0];
+
+    chrome.tabs.sendMessage(currentTab.id, { action: 'filter' }, response => {
+        console.log('Filter action sent');
+    });
+});
+
+const sendFilterData = e => {
+    e.preventDefault();
+
+    
+}
+
